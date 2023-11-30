02da9e893fcae2b8559f1767675ad01bf2329207
Neo40 <tron36.cc@gmail.com>
2023-11-30 16:37:54 -0300

    Geração de getters and setters

--- src/Vendedor.java
+++ src/Vendedor.java
@@ -8,4 +8,59 @@ public class Vendedor {
     private Double valorBonificacao;
 
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public Double getValorSalario() {
+        return valorSalario;
+    }
+
+    public void setValorSalario(Double valorSalario) {
+        this.valorSalario = valorSalario;
+    }
+
+    public Double getValorBonificacao() {
+        return valorBonificacao;
+    }
+
+    public void setValorBonificacao(Double valorBonificacao) {
+        this.valorBonificacao = valorBonificacao;
+    }
 }
