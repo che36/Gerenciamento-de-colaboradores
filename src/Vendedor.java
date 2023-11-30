0ac52be271087d1ecff61112f51575b28ab2a0b5
Neo40 <tron36.cc@gmail.com>
2023-11-30 16:03:05 -0300

    Adicao de classe Vendedor

---
+++ src/Vendedor.java
@@ -0,0 +1,11 @@
+public class Vendedor {
+    String nome;
+    String documento;
+    String rua;
+    String bairro;
+    String complemento;
+    Double valorSalario;
+    Double valorBonificacao;
+
+
+}
