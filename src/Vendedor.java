60e51ecd90b11c6f8713f3f11b673de436f7102d
Neo40 <tron36.cc@gmail.com>
2023-12-02 09:29:56 -0300

    Criação de construtor da classe com referencia super

--- src/Vendedor.java
+++ src/Vendedor.java
@@ -1,51 +1,13 @@
-public class Vendedor {
-    private String nome;
-    private String documento;
-    private Endereco endereco;
-    private Double valorSalario;
+public class Vendedor extends FuncionarioCLT {
     private Double valorBonificacao;
-
-    public Vendedor(String nome, String documento, Endereco endereco, Double valorSalario) {
-        this.nome = nome;
-        this.documento = documento;
-        this.endereco = endereco;
-        this.valorSalario = valorSalario;
-    }
 
     public Vendedor() {
 
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-    public void setDocumento(String documento) {
-        this.documento = documento;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
-
-    public Double getValorSalario() {
-        return valorSalario;
-    }
-
-    public void setValorSalario(Double valorSalario) {
-        this.valorSalario = valorSalario;
+    public Vendedor(String nome, String documento, Endereco endereco, Double valorSalario, Double valorBonificacao) {
+        super(nome, documento, endereco, valorSalario);
+        this.valorBonificacao = valorBonificacao;
     }
 
     public Double getValorBonificacao() {
