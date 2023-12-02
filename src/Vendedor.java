fc9194fc3aeb925ead312c27ec0885cec7ef91d3
Neo40 <tron36.cc@gmail.com>
2023-12-02 09:58:41 -0300

    remoção do atributo valorBonificação,por ser varíavel

--- src/Vendedor.java
+++ src/Vendedor.java
@@ -24,7 +24,6 @@ public class Vendedor extends FuncionarioCLT {
                 ", documento='" + documento + '\'' +
                 ", endereco=" + endereco +
                 ", valorSalario=" + valorSalario +
-                "valorBonificacao=" + valorBonificacao +
                 '}';
     }
 }
