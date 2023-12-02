c9a0c61f5489fcd4ac902ffab2e375fb402ac2f5
Neo40 <tron36.cc@gmail.com>
2023-12-02 09:36:33 -0300

    Criação de 2 construtores da classe com referencia super

--- src/OperadordeCaixa.java
+++ src/OperadordeCaixa.java
@@ -1,8 +1,9 @@
-public class OperadordeCaixa {
-    String nome;
-    String documento;
-    String rua;
-    String bairro;
-    String complemento;
-    Double valorSalario;
+public class OperadordeCaixa extends FuncionarioCLT {
+
+    public OperadordeCaixa(String nome, String documento, Endereco endereco, Double valorSalario) {
+        super(nome, documento, endereco, valorSalario);
+    }
+
+    public OperadordeCaixa() {
+    }
 }
